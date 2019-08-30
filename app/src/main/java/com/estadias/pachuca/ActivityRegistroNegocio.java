ae8edcd807644f55c2b053055d79eba2fa4dbd08
EstebanIslas <1717110261@utectulancingo.edu.mx>
2019-08-29 19:51:23 -0500

    Se comenta el resgitro y ahora es estatico

--- app/src/main/java/com/estadias/pachuca/ActivityRegistroNegocio.java
+++ app/src/main/java/com/estadias/pachuca/ActivityRegistroNegocio.java
@@ -11,6 +11,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.AdapterView;
@@ -36,7 +37,7 @@ import java.util.regex.Pattern;
 
 public class ActivityRegistroNegocio extends AppCompatActivity {
 
-    EditText edt_nombre_reg_negocio;
+    /*EditText edt_nombre_reg_negocio;
     EditText edt_correo_reg_negocio;
     EditText edt_calle_reg_negocio;
     EditText edt_colonia_reg_negocio;
@@ -69,7 +70,11 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
 
     //Seleccionar Imagen
     Bitmap bitmap;
-    private static final int COD_SELECCIONA = 10;
+    private static final int COD_SELECCIONA = 10;*/
+
+    Button btn_whats;
+    Button btn_mapa;
+    Button btn_llamar;
 
 
     @Override
@@ -77,7 +82,32 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_negocio);
 
-        initComponents();
+        btn_llamar = findViewById(R.id.btn_llamar_registro);
+        btn_whats = findViewById(R.id.btn_mensaje_registro);
+        btn_mapa = findViewById(R.id.btn_mapa_registro);
+
+        btn_whats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mensajeOnClick(view);
+            }
+        });
+
+        btn_llamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                llamarOnClick(view);
+            }
+        });
+
+        btn_mapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vermapaOnClick(view);
+            }
+        });
+
+        /*initComponents();
 
         request = Volley.newRequestQueue(this);
 
@@ -112,10 +142,48 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
             public void onClick(View view) {
                 mostrarDialogOpciones();
             }
-        });
+        });*/
     }
 
-    private void initComponents() {
+    private void mensajeOnClick(View view) {
+        /*Uri mensaje = Uri.parse("smsto:"+ tv_telefono_consulta_neg.getText().toString());
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, mensaje);
+        smsIntent.putExtra("sms_body", "");
+
+        startActivity(smsIntent);*/
+        try{
+            String telefono_consulta = "7711530636";
+            String numero = "+521" + telefono_consulta;
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            String uri = "whatsapp://send?phone=" + numero;
+            intent.setData(Uri.parse(uri));
+            startActivity(intent);
+        }catch (Exception ex) {
+            Toast.makeText(this, "Ocurrió un problema con WhatsApp", Toast.LENGTH_SHORT).show();
+            Log.e("\n\nERROR: " , ex.getMessage());
+        }
+    }
+
+    private void llamarOnClick(View view) {
+        Uri numero = Uri.parse("tel:"+ "7711530636");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, numero);
+
+        startActivity(callIntent);
+    }
+
+    private void vermapaOnClick(View view){
+        try {
+            String ubicacion = "https://www.google.com/maps/place/Sargon+-+Distribuidor+Aspel+Soft+Restaurant+C%C3%A1maras+de+Seguridad+P%C3%A1ginas+Web+Pachuca+Hidalgo/@20.1227128,-98.747726,17z/data=!3m1!4b1!4m5!3m4!1s0x85d109f8546f0265:0x5be57bf68b9230d8!8m2!3d20.122866!4d-98.745469?hl=es";
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(ubicacion));
+            intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+            startActivity(intent);
+        }catch (Exception ex){
+            Toast.makeText(this, "Ocurrió un problema con Google Maps", Toast.LENGTH_SHORT).show();
+            Log.e("\n\nERROR: " , ex.getMessage());
+        }
+    }
+
+    /*private void initComponents() {
         edt_nombre_reg_negocio = findViewById(R.id.edt_nombre_reg_negocio);
         edt_correo_reg_negocio = findViewById(R.id.edt_correo_reg_negocio);
         edt_calle_reg_negocio = findViewById(R.id.edt_calle_reg_negocio);
@@ -130,17 +198,17 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
 
         btn_registrar_negocio = findViewById(R.id.btn_registrar_negocio);
 
-        //*** Seleccionar Imagen ***//
+
 
         img_imagen_reg_negocio = findViewById(R.id.img_imagen_reg_negocio);
         btn_seleccionar_imagen_negocio = findViewById(R.id.btn_seleccionar_imagen_negocio);
 
         //Spiner para categoria
         spin_categoria_reg_negocio = findViewById(R.id.spin_categoria_reg_negocio);
-    }
+    }*/
 
 
-    private void limpiarEditText() {
+    /*private void limpiarEditText() {
 
         edt_nombre_reg_negocio.setText("");
         edt_correo_reg_negocio.setText("");
@@ -154,10 +222,10 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
         edt_municipio_reg_negocio.setText("");
         edt_confirm_pass_reg_negocio.setText("");
 
-    }
+    }*/
 
 
-    private void cargarWebservice() {
+    /*private void cargarWebservice() {
 
         progreso = new ProgressDialog(this);
         progreso.setMessage("Conectando...");
@@ -289,9 +357,9 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
             Toast.makeText(this, "No se pudo registrar el negocio intentelo mas tarde", Toast.LENGTH_SHORT).show();
             progreso.hide();
         }
-    }
+    }*/
 
-    private String convertitImgString(Bitmap bitmap) {
+    /*private String convertitImgString(Bitmap bitmap) {
 
         ByteArrayOutputStream array = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG,100,array); //Comprime el bitmap de el valor de la imagen seleccionada
@@ -299,11 +367,11 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
         String imagenString = Base64.encodeToString(imageByte, Base64.DEFAULT); //Se retorna la imagen en base64
 
         return imagenString;
-    }
+    }*/
 
 
 
-    //Metodos para cargar la imagen desde la galeria
+    /*/Metodos para cargar la imagen desde la galeria
 
     private void mostrarDialogOpciones() {
         final CharSequence[] opciones = {"Elegir de Galeria", "Cancelar"}; //Se crea una secuencia de opciones
@@ -347,12 +415,12 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
         }catch (Exception err){
             Toast.makeText(this, "No ha seleccionado la imagen", Toast.LENGTH_SHORT).show();
         }
-    }
+    }*/
 
 
     //########### Encriptar la contraseña mediante MD5 ###########
 
-    private static String getHash(String txt, String hashType) {
+    /*private static String getHash(String txt, String hashType) {
         try {
             java.security.MessageDigest md = java.security.MessageDigest
                     .getInstance(hashType);
@@ -367,22 +435,22 @@ public class ActivityRegistroNegocio extends AppCompatActivity {
             System.out.println(e.getMessage());
         }
         return null;
-    }
+    }*/
 
-    /* Retorna un hash MD5 a partir de un texto */
+    /* Retorna un hash MD5 a partir de un texto
     private static String md5(String txt) {
         return getHash(txt, "MD5");
-    }
+    }*/
 
-    /* Retorna un hash SHA1 a partir de un texto */
+    /* Retorna un hash SHA1 a partir de un texto
     private static String sha1(String txt) {
         return getHash(txt, "SHA1");
-    }
+    }*/
 
-    /* Valida el correo electronico  */
+    /* Valida el correo electronico
     private boolean validarEmail(String email){
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         return pattern.matcher(email).matches();
-    }
+    }*/
 
 }
